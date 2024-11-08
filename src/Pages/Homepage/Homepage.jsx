67cbd841c79e1410df7ecc28faf1310fdfb49a32
Emirlan Dogdurbaev <jigit38@gmail.com>
2024-11-08 18:28:12 +0600

    make special product slider

--- src/Pages/Homepage/Homepage.jsx
+++ src/Pages/Homepage/Homepage.jsx
@@ -2,13 +2,14 @@ import classes from './Homepage.module.scss'
 import Header from "../../components/Header/Header.jsx";
 import Counter from "../../components/Counter/Counter.jsx";
 import CompanyGoalHome from "../../components/CompanyGoalHome/CompanyGoalHome.jsx";
+import SpecialProductCard from "../../components/SpecialProductCard/SpecialProductCard.jsx";
 const Homepage = () => {
     return (
         <main className={classes.HomePage}>
             <Header/>
             <Counter/>
             <CompanyGoalHome/>
-            <p className='commit'>Комит как Амит ахахах</p>
+            <SpecialProductCard/>
         </main>
     )
 }
---
+++ src/components/SpecialProductCard/SpecialProductCard.jsx
@@ -0,0 +1,67 @@
+import styles from "./SpecialProductCard.module.scss";
+import CardItem from "./CardItem/CardItem.jsx";
+import {Swiper, SwiperSlide} from "swiper/react";
+import {Navigation, Pagination} from "swiper/modules";
+
+const SpecialProductCard = () => {
+    const productData = [
+        {id: 1, name: 'Product 1', price: '$10', description: 'Description 1'},
+        {id: 2, name: 'Product 2', price: '$20', description: 'Description 2'},
+        {id: 1, name: 'Product 1', price: '$10', description: 'Description 1'},
+        {id: 2, name: 'Product 2', price: '$20', description: 'Description 2'},
+    ];
+
+    return (
+        <section className={styles.SpecialProductCard}>
+            <div className={styles.title}>
+                <h2>Специальное предложение</h2>
+            </div>
+
+            <div className={styles.slider_container}>
+                <span className={styles.btn_cont}>
+                    <button className={styles.arrow_left}>axax</button>
+                    <button className={styles.arrow_right}>axax</button>
+                </span>
+
+                <Swiper
+                    modules={[Navigation, Pagination]}
+                    navigation={{
+                        nextEl: `.${styles.arrow_right}`,
+                        prevEl: `.${styles.arrow_left}`
+                    }}
+                    spaceBetween={30}
+                    slidesPerView={"auto"}
+                    className={styles.slides}
+                    breakpoints={{
+                        1440: {
+                            slidesPerView: 2,
+                        },
+                        1280: {
+                            slidesPerView: 1,
+                        },
+                        1024: {
+                            slidesPerView: 1,
+                        },
+                        800: {
+                            slidesPerView: 1,
+                        },
+                        768: {
+                            slidesPerView: 1,
+                        },
+                        600: {
+                            slidesPerView: 1,
+                        }
+                    }}>
+                    {productData.map((product, index) => (
+                        <SwiperSlide key={index} className={styles.slide}>
+                            <CardItem {...product} />
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+
+            </div>
+        </section>
+    )
+}
+
+export default SpecialProductCard
