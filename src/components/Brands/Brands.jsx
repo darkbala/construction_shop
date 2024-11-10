f72018ca11c1f59dd3117b3b9677e3ea63121506
Emirlan Dogdurbaev <jigit38@gmail.com>
2024-11-10 23:57:24 +0600

    test size 02

--- src/components/Brands/Brands.jsx
+++ src/components/Brands/Brands.jsx
@@ -1,13 +1,19 @@
 import styles from './Brands.module.scss';
 import img from "../../assets/img.png"
+import logo1 from "../../assets/logo1.png"
+import logo2 from "../../assets/logo2.png"
+import logo3 from "../../assets/logo3.png"
+import logo4 from "../../assets/logo4.png"
+import logo5 from "../../assets/logo5.png"
+import logo6 from "../../assets/logo6.png"
 
 const logos = [
-    `${img}`,
-    `${img}`,
-    `${img}`,
-    `${img}`,
-    `${img}`,
-    `${img}`,
+    `${logo1}`,
+    `${logo2}`,
+    `${logo3}`,
+    `${logo4}`,
+    `${logo5}`,
+    `${logo6}`,
 ];
 
 const Brands = () => {
