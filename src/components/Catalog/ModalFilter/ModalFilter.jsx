40369f2ba4df4707e531ffb2d7aabf0f404556d6
darkbala <rustam.samsaev@gmail.com>
2024-11-17 02:23:18 +0600

    sinchronize input values with slider

--- src/components/Catalog/ModalFilter/ModalFilter.jsx
+++ src/components/Catalog/ModalFilter/ModalFilter.jsx
@@ -24,10 +24,26 @@ export default function ModalFilter({ onClose }) {
     setMaxValue(value[1]);
   };
 
+  const handleMinInputChange = (e) => {
+    const value = Math.max(0, Number(e.target.value.replace(/\D/g, ''))); // Убираем все нецифровые символы
+    setMinValue(value);
+  };
+
+  const handleMaxInputChange = (e) => {
+    const value = Math.max(0, Number(e.target.value.replace(/\D/g, ''))); // Убираем все нецифровые символы
+    setMaxValue(value);
+  };
+
   const handleFormSubmit = (e) => {
     e.preventDefault();
     // Логика отправки или фильтрации
     console.log('Filters submitted', { minValue, maxValue, productType });
+  };
+
+  // Функция для форматирования чисел
+  const formatNumber = (number) => {
+    return number.toLocaleString('ru-RU'); // Для пробела между тысячами
+    // return number.toLocaleString('de-DE'); // Для точки между тысячами
   };
 
   return (
@@ -43,19 +59,27 @@ export default function ModalFilter({ onClose }) {
                 min={0}
                 max={12000000}
                 step={1000}
-                defaultValue={[minValue, maxValue]}
+                value={[minValue, maxValue]}
                 onChange={handleSliderChange}
-                tipFormatter={(value) => `${value}`} // Форматирование подсказки
+                tipFormatter={(value) => formatNumber(value)} // Форматирование подсказки
               />
             </div>
             <div className={styles.vals}>
               <div>
                 <p>Мин.</p>
-                <input type="text" disabled value={minValue} />
+                <input 
+                  type="text" 
+                  value={formatNumber(minValue)} 
+                  onChange={handleMinInputChange} 
+                />
               </div>
               <div>
                 <p>Макс.</p>
-                <input type="text" disabled value={maxValue} />
+                <input 
+                  type="text" 
+                  value={formatNumber(maxValue)} 
+                  onChange={handleMaxInputChange} 
+                />
               </div>
             </div>
             <div className={styles.radios}>
