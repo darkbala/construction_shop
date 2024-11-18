ad4ea76715d22efc4c03679c9f759de6b8ccff11
Emirlan Dogdurbaev <jigit38@gmail.com>
2024-11-19 03:47:05 +0600

    added all vacancy get oage for admin

--- src/Pages/AdminPage/AdminPage.jsx
+++ src/Pages/AdminPage/AdminPage.jsx
@@ -3,15 +3,21 @@ import styles from "./AdminPage.module.scss";
 import AllProducts from "../../components/AllProducts/AllProducts.jsx";
 import AllCollections from "../../components/AllCollections/AllCollections.jsx";
 import ChangeCollection from "../../components/ChangeCollection/ChangeCollection.jsx";
+import AddCategory from "../../components/AddCategory/AddCategory.jsx";
+import AllVacancy from "../../components/AllVacancy/AllVacancy.jsx";
 
 const AdminPage = () => {
     return (
         <div className={styles.admin}>
-            {/*<ModifySpecialOffer/>*/}
-            {/*<AllProducts/>*/}
-            {/*<AllCollections/>*/}
+            <ModifySpecialOffer/>
+            <AllProducts/>
+            <AllCollections/>
 
             <ChangeCollection/>
+
+            <AddCategory/>
+
+            <AllVacancy/>
         </div>
     )
 }
