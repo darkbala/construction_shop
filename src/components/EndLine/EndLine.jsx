a8f143e40ea61a87e08d62a0439e8399f5838dbf
darkbala <rustam.samsaev@gmail.com>
2024-11-18 18:20:43 +0600

    endline adaprive

--- src/components/EndLine/EndLine.jsx
+++ src/components/EndLine/EndLine.jsx
@@ -7,16 +7,18 @@ import whats from '../../assets/whats.png'
 export default function EndLine() {
   return (
     <div className={styles.wrap}>
-      <div className={styles.left}>
-        <img src={logo} alt="logo" className={styles.logo} />
-        <div className={styles.down}>
-          <p className={styles.big}>Первый отечественный бренд на рынке сантехники от компании ОсОО “Стройдом.кг”</p>
-          <p className={styles.small}>© 2023 Iskender.kg - Отечественный бренд сантехники</p>
+      <div className={styles.content}>
+        <div className={styles.left}>
+          <img src={logo} alt="logo" className={styles.logo} />
+          <div className={styles.down}>
+            <p className={styles.big}>Первый отечественный бренд на рынке сантехники от компании ОсОО “Стройдом.кг”</p>
+            <p className={styles.small}>© 2023 Iskender.kg - Отечественный бренд сантехники</p>
+          </div>
         </div>
-      </div>
-      <div className={styles.right}>
-        <img src={insta} alt="instagram" />
-        <img src={whats} alt="whatsapp" />
+        <div className={styles.right}>
+          <img src={insta} alt="instagram" />
+          <img src={whats} alt="whatsapp" />
+        </div>
       </div>
     </div>
   )
